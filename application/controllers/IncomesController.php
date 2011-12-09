77110fbaff59869056765f32879f7bb30fb82978
Eli Hayun <elihayun@gmail.com>
2011-12-09 05:20:50 +0200

    Incomes

--- application/controllers/IncomesController.php
+++ application/controllers/IncomesController.php
@@ -5,7 +5,7 @@ class IncomesController extends Vaad_Controller_Action {
         public function init() {
         $this->tbl = new Vaad_DbTable_Incomes();
         $this->select = $this->tbl->select();
-        $this->select->order('inc_apt_num');
+        $this->select->order('inc_apt_num')->order('inc_date');
         $this->form = new Vaad_Form_Income();
         $this->_name = 'הכנסות';
     }
